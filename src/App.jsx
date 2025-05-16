e703a25dc92b525ac73670693fe236ed1a6e7f51
ImThePeak <abavi10@gmail.com>
2025-05-16 18:00:33 +0200

    login page implemented

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Welcome from './components/Welcome'
+
+function App() {
+  return (
+    <div>
+      <h1>Meme Game</h1>
+      <Welcome />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Welcome.jsx
@@ -0,0 +1,10 @@
+function Welcome(){
+    return (
+      <div>
+        <h1>Welcome to the Meme Game</h1>
+        <p>This is a game where you have to guess the meme</p>
+      </div>
+    )
+  }
+
+export default Welcome;
